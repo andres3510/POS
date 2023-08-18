8c2c7c9333da982f79919a299ebe54c97495efc7
Luis andrés Ríos noreña <andres.3510@hotmail.com>
2023-08-18 17:03:32 -0500

    primera version

---
+++ POS.Infrastructure/Persistences/Context/POSContext.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace POS.Domain.Entities
+{
+    public partial class POSContext : DbContext
+    {
+        public POSContext()
+        {
+        }
+
+        public POSContext(DbContextOptions<POSContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<BranchOffice> BranchOffices { get; set; } = null!;
+        public virtual DbSet<Business> Businesses { get; set; } = null!;
+        public virtual DbSet<Category> Categories { get; set; } = null!;
+        public virtual DbSet<Client> Clients { get; set; } = null!;
+        public virtual DbSet<Department> Departments { get; set; } = null!;
+        public virtual DbSet<District> Districts { get; set; } = null!;
+        public virtual DbSet<DocumentType> DocumentTypes { get; set; } = null!;
+        public virtual DbSet<Menu> Menus { get; set; } = null!;
+        public virtual DbSet<MenuRole> MenuRoles { get; set; } = null!;
+        public virtual DbSet<Product> Products { get; set; } = null!;
+        public virtual DbSet<Provider> Providers { get; set; } = null!;
+        public virtual DbSet<Province> Provinces { get; set; } = null!;
+        public virtual DbSet<Purcharse> Purcharses { get; set; } = null!;
+        public virtual DbSet<PurcharseDetail> PurcharseDetails { get; set; } = null!;
+        public virtual DbSet<Role> Roles { get; set; } = null!;
+        public virtual DbSet<Sale> Sales { get; set; } = null!;
+        public virtual DbSet<SaleDetail> SaleDetails { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+        public virtual DbSet<UserRole> UserRoles { get; set; } = null!;
+        public virtual DbSet<UsersBranchOffice> UsersBranchOffices { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-FKFKN0T\\SQLEXPRESS;Database=POS;Trusted_connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<SaleDetail>(entity =>
+            {
+                entity.Property(e => e.Discount).HasColumnType("decimal(18, 2)");
+
+                entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+
+                entity.HasOne(d => d.Product)
+                    .WithMany(p => p.SaleDetails)
+                    .HasForeignKey(d => d.ProductId)
+                    .HasConstraintName("FK__SaleDetai__Produ__6A30C649");
+
+                entity.HasOne(d => d.Sale)
+                    .WithMany(p => p.SaleDetails)
+                    .HasForeignKey(d => d.SaleId)
+                    .HasConstraintName("FK__SaleDetai__SaleI__6B24EA82");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.Property(e => e.Email).IsUnicode(false);
+
+                entity.Property(e => e.Image).IsUnicode(false);
+
+                entity.Property(e => e.Password).IsUnicode(false);
+
+                entity.Property(e => e.UserName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+
+            modelBuilder.Entity<UserRole>(entity =>
+            {
+                entity.HasOne(d => d.BranchOffice)
+                    .WithMany(p => p.UserRoles)
+                    .HasForeignKey(d => d.BranchOfficeId)
+                    .HasConstraintName("FK__UserRoles__Branc__6E01572D");
+
+                entity.HasOne(d => d.Role)
+                    .WithMany(p => p.UserRoles)
+                    .HasForeignKey(d => d.RoleId)
+                    .HasConstraintName("FK__UserRoles__RoleI__6EF57B66");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.UserRoles)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__UserRoles__UserI__6FE99F9F");
+            });
+
+            modelBuilder.Entity<UsersBranchOffice>(entity =>
+            {
+                entity.HasKey(e => e.UserBranchOfficeId)
+                    .HasName("PK__UsersBra__7D1E804A7F297B39");
+
+                entity.HasOne(d => d.BranchOffice)
+                    .WithMany(p => p.UsersBranchOffices)
+                    .HasForeignKey(d => d.BranchOfficeId)
+                    .HasConstraintName("FK__UsersBran__Branc__70DDC3D8");
+
+                entity.HasOne(d => d.User)
+                    .WithMany(p => p.UsersBranchOffices)
+                    .HasForeignKey(d => d.UserId)
+                    .HasConstraintName("FK__UsersBran__UserI__71D1E811");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
